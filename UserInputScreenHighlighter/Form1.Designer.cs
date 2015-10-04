0a31f65e0a73e2a10d1a9f4a362802b5c0287179
Allen Thomas Varghese <allentv4u@gmail.com>
2015-10-04 03:38:20 +0100

    Initial Commit

---
+++ UserInputScreenHighlighter/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            // 
+            // label1
+            // 
+            this.label1.CausesValidation = false;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(284, 74);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Waiting...";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.Border;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CausesValidation = false;
+            this.ClientSize = new System.Drawing.Size(284, 74);
+            this.Controls.Add(this.label1);
+            this.Enabled = false;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Opacity = 0.9D;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.Activated += new System.EventHandler(this.Form1_Activated);
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ UserInputScreenHighlighter/Form1.cs
@@ -0,0 +1,188 @@
+﻿/**
+ * Author: Allen Thomas Varghese
+ * Date : 04-October-2015
+ * Adapted from http://blogs.msdn.com/b/toub/archive/2006/05/03/589423.aspx
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        private MenuItem showMenuItem;
+        private MenuItem hideMenuItem;
+        private MenuItem exitMenuItem;
+
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private const int WM_SYSKEYUP = 0x0105;    // Modifier Keys
+        private const int WM_SYSKEYDOWN = 0x0104;  // ALT Key
+        private static LowLevelKeyboardProc _proc = HookCallback;
+        private static IntPtr _hookID = IntPtr.Zero;
+
+        private static Label keyPressMessage;
+        private static String prevMessage = "", currMessage = "";
+
+        public Form1()
+        {
+            _hookID = SetHook(_proc);
+            InitializeComponent();
+            keyPressMessage = label1;
+            SetNotifyIconProperties();
+        }
+
+        /**
+         * Set properties for the Notify Icon in the System Tray
+         */
+        private void SetNotifyIconProperties()
+        {
+            this.notifyIcon1.Icon = Form1.ActiveForm.Icon;
+            this.notifyIcon1.ContextMenu = new ContextMenu();
+            showMenuItem = new MenuItem("Show", new EventHandler(HandleNotifyIconClick));
+            hideMenuItem = new MenuItem("Hide", new EventHandler(HandleNotifyIconClick));
+            exitMenuItem = new MenuItem("Exit", new EventHandler(HandleNotifyIconClick));
+            this.notifyIcon1.ContextMenu.MenuItems.Add(showMenuItem);
+            this.notifyIcon1.ContextMenu.MenuItems.Add(hideMenuItem);
+            this.notifyIcon1.ContextMenu.MenuItems.Add(exitMenuItem);
+        }
+
+        /**
+         * Event Handler for the Notify Icon
+         */
+        private void HandleNotifyIconClick(object sender, EventArgs e)
+        {
+            if (sender == showMenuItem)
+            {
+                Console.WriteLine("Show Menu Item Clicked!");
+                this.Show();
+                showMenuItem.Enabled = false;
+                hideMenuItem.Enabled = true;
+            }
+            else if (sender == hideMenuItem)
+            {
+                Console.WriteLine("Hide Menu Item Clicked!");
+                this.Hide();
+                showMenuItem.Enabled = true;
+                hideMenuItem.Enabled = false;
+            }
+            else if (sender == exitMenuItem)
+            {
+                Console.WriteLine("Exit Menu Item Clicked!");
+                this.Close();
+            }
+        }
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        // Handle mouse click throughs
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams createParams = base.CreateParams;
+                createParams.ExStyle |= 0x00000020; // WS_EX_TRANSPARENT
+
+                return createParams;
+            }
+        }
+
+        private static IntPtr SetHook(LowLevelKeyboardProc proc)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, proc,
+                    GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+        private delegate IntPtr LowLevelKeyboardProc(
+            int nCode, IntPtr wParam, IntPtr lParam);
+
+        private static IntPtr HookCallback(
+            int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && (wParam == (IntPtr)WM_KEYDOWN || wParam == (IntPtr)WM_SYSKEYUP || wParam == (IntPtr)WM_SYSKEYDOWN))
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+                //Console.WriteLine((Keys)vkCode);
+                ProcessUserKeyPress((Keys)vkCode);
+            }
+            return CallNextHookEx(_hookID, nCode, wParam, lParam);
+        }
+
+        // Process user pressed keys
+        private static void ProcessUserKeyPress(Keys keys)
+        {
+            //Console.WriteLine("prevMessage: " + prevMessage + "  currMessage: " + currMessage);
+            if (keys.ToString().Equals("LControlKey") || keys.ToString().Equals("RControlKey"))
+            {
+                currMessage = "Ctrl";
+            }
+            else if (keys.ToString().Equals("LShiftKey") || keys.ToString().Equals("RShiftKey"))
+            {
+                currMessage = "Shift";
+            }
+            else if (keys.ToString().Equals("LMenu") || keys.ToString().Equals("RMenu"))
+            {
+                currMessage = "Alt";
+            }
+            else
+            {
+                currMessage = keys.ToString();
+            }
+            // Check for modifier combinations
+            if("+Shift+Ctrl+Alt+".Contains(prevMessage))
+            {
+                if (!prevMessage.Equals(currMessage) && !prevMessage.Equals(""))
+                {
+                    currMessage = prevMessage + "+" + currMessage;
+                }
+            }
+            keyPressMessage.Text = currMessage;
+            prevMessage = currMessage;
+        }
+        
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook,
+            LowLevelKeyboardProc lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
+            IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        private void Form1_Activated(object sender, EventArgs e)
+        {
+            this.Location = new Point()
+            {
+                X = (Screen.PrimaryScreen.WorkingArea.Width / 2 - this.Width / 2),
+                Y = 0
+            };
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            UnhookWindowsHookEx(_hookID);
+        }
+
+    }
+}
